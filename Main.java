b88f120f34281fcc7a19a5582dcc909c4b095a50
Jose Manuel Pérez <jmperez27@gmail.com>
2021-03-30 12:25:28 +0200

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,30 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package jaen;
-
-//procedimiento ensayar (paso : TipoPaso)
-
 import java.util.Scanner;
-
-//  repetir
-//  |  seleccionar_candidato
-//  |  if aceptable then
-//  |  begin
-//  |    anotar_candidato
-//  |    if solucion_incompleta then
-//  |    begin
-//  |      ensayar(paso_siguiente)
-//  |      if no acertado then borrar_candidato
-//  |    end
-//  |    else begin
-//  |      anotar_solucion
-//  |      acertado <- cierto;
-//  |  end
-//  hasta que (acertado = cierto) o (candidatos_agotados)
-//fin procedimiento
 
 /**
  *
@@ -34,65 +8,56 @@ public class Main {
     
     public static int buscarMax(char[][] mapa, int fila, int columna) {
                
-        //si no es posible solución
+        //casos base
         if (fila < 0 || columna < 0 || fila+1>mapa.length || columna+1 > mapa[0].length || mapa[fila][columna]== ' ') 
             return 0;
         else
         {
+            //marcamos el paso
             mapa[fila][columna] = ' ';
-            return 1
-                    + buscarMax(mapa, fila-1, columna) 
-                    + buscarMax(mapa, fila+1, columna) 
-                    + buscarMax(mapa, fila, columna-1) 
-                    + buscarMax(mapa, fila, columna+1);
+            //import
+            return 1 + buscarMax(mapa, fila-1, columna) + buscarMax(mapa, fila+1, columna) + buscarMax(mapa, fila, columna-1) + buscarMax(mapa, fila, columna+1);
         }        
-    }
-    
-    public static int buscar(char[][] mapa) {        
-        int actual, max = 0;
-        for (int fila=0; fila<mapa.length; fila++) {
-            for (int columna=0; columna<mapa.length;columna++) {                                
-                if (mapa[fila][columna] == '#') {
-                    actual = buscarMax(mapa, fila, columna);
-                    if (actual > max)
-                        max = actual;
-                }                
-            }
-        }
-        
-        return max;
     }
     
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        
         Scanner sc = new Scanner(System.in);
          
-        int filas, columnas, max;
+        int filas, columnas, max, actual;
         String linea;
-        //leer linea   
         
-        
-        
-        while (sc.hasNext()) {
+        while (sc.hasNext()) 
+        {
             filas = sc.nextInt();
             columnas = sc.nextInt();
+            sc.nextLine();
+
             char[][] mapa = new char[filas][columnas];
-            
+
+            for (int i = 0; i < filas; i++)
+            {
+                    linea = sc.nextLine();
+                    for (int j = 0; j < columnas; j++)
+                    {
+                            mapa[i][j] = linea.charAt(j);
+                    }
+            }                  
+            max = 0;
             for (int i=0; i<filas; i++) {
-                linea = sc.nextLine();
-                for (int j=0; j<linea.length();j++)
-                    mapa[i][j]=linea.charAt(j);
+                for (int j=0; j<columnas; j++) {                                
+                    if (mapa[i][j] == '#') {
+                        actual = buscarMax(mapa, i, j);
+                        if (actual > max)
+                            max = actual;
+                    }
+                }                
             }           
-            
-            max = buscar(mapa);
-            
-        }
-        
-        
-        
-    }
-    
+            System.out.println(max);
+            }           
+    }    
 }
+
